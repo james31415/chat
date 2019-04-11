ad4141e7a4ca371ad56c5cbd171eb66704557e5d
James Hall <hallj@opentext.com>
2019-04-11 14:22:49 -0400

    Permit different versions of Visual Studio

--- src/build.bat
+++ src/build.bat
@@ -1,6 +1,20 @@
 @echo off
 setlocal
-call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat" > nul
+
+if exist "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat" (
+	call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat" > nul
+	goto run
+)
+
+if exist "C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\VC\Auxiliary\Build\vcvars64.bat" (
+	call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\VC\Auxiliary\Build\vcvars64.bat" > nul
+	goto run
+)
+
+echo Cannot setup environment
+exit /b 1
+
+:run
 if not exist ..\bin mkdir ..\bin
 
 set CommonCompilerFlags=-nologo -W4 -Od -wd4996
